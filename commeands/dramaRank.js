e3ad5dd646a23715d6e877554ccfa4cb36ee8fb1
PCdoremiso <flowerway626@gmail.com>
2022-11-29 23:01:46 +0800

    刪除小 bug

--- commeands/dramaRank.js
+++ commeands/dramaRank.js
@@ -13,30 +13,30 @@ export default async (event) => {
     const { data } = await axios.get(url)
     const $ = cheerio.load(data)
     console.log(url)
+    console.log($)
     const dramas = []
-    if ($('.box_inner').find('ul').text() !== '') {
-      $('.category-list li').each(function (i) {
-        const replyFlex = JSON.parse(JSON.stringify(flexRank))
-        replyFlex.body.contents[0].url = $(this).find('.movie_foto img').attr('src')
-        replyFlex.body.contents[0].action.text = $(this).find('.movielist_info h2').text().trim()
-        replyFlex.body.contents[3].contents[0].contents[0].contents[0].text = $(this).find('.movielist_info h2').text().trim()
-        replyFlex.body.contents[3].contents[0].contents[1].contents[0].contents[0].text = $(this).find('.movielist_info .season').text().trim()
-        replyFlex.body.contents[3].contents[0].contents[1].contents[1].contents[0].contents[0].text = 'NO.' + (i + 1)
-        replyFlex.body.contents[1].contents[0].contents[0].text = event.message.text
-        dramas.push(replyFlex)
-      })
-      const reply = {
-        type: 'flex',
-        altText: '排行查詢結果',
-        contents: {
-          type: 'carousel',
-          contents: dramas
-        }
+    $('.category-list li').each(function (i) {
+      const replyFlex = JSON.parse(JSON.stringify(flexRank))
+      replyFlex.body.contents[0].url = $(this).find('.movie_foto img').attr('src')
+      replyFlex.body.contents[0].action.text = $(this).find('.movielist_info h2').text().trim()
+      replyFlex.body.contents[3].contents[0].contents[0].contents[0].text = $(this).find('.movielist_info h2').text().trim()
+      replyFlex.body.contents[3].contents[0].contents[1].contents[0].contents[0].text = $(this).find('.movielist_info .season').text().trim()
+      replyFlex.body.contents[3].contents[0].contents[1].contents[1].contents[0].contents[0].text = 'NO.' + (i + 1)
+      replyFlex.body.contents[1].contents[0].contents[0].text = event.message.text
+      dramas.push(replyFlex)
+    })
+    const reply = {
+      type: 'flex',
+      altText: '排行查詢結果',
+      contents: {
+        type: 'carousel',
+        contents: dramas
       }
+    }
 
-      await event.reply(reply)
-      writejson(reply, 'dramaRank')
-    } else await event.reply('查無資料，請更換檢索條件')
+    await event.reply(reply)
+    writejson(reply, 'dramaRank')
+    await event.reply('查無資料，請更換檢索條件')
     // return dramaNums
   } catch (error) {
     await event.reply('發生錯誤，請稍後再試')
